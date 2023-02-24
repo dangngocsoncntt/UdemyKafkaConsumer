d71d8f604408a9d47d584343155a91a219d77a66
sondn <dangngocsoncntt@gmail.com>
2023-02-24 08:56:26 +0700

    cap nhat lan dau tien

---
+++ src/main/java/com/learnkafka/learnkafkaconsumer/LearnkafkaconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.learnkafka.learnkafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnkafkaconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnkafkaconsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnkafka/learnkafkaconsumer/LearnkafkaconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnkafka.learnkafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnkafkaconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
